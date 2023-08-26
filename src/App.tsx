a3517a27e18ed0c5331f610303752b93123bbe27
Vivek Kumar <vyvivekkumar@gmail.com>
2023-08-26 23:40:33 +0530

    cses problem counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import cses from "./../cses.json";
+
+const csesWebAddress = "https://cses.fi/problemset/task/";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const randomIndex = () => Math.floor(Math.random() * cses.problems.length);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const [index, setIndex] = useState(randomIndex);
+    const [solvedCount, setSolvedCount] = useState(0);
+    const [solved, setSolved] = useState(new Set());
+
+    const settingIndex = () => {
+        let curProblemId = randomIndex();
+        while (solved.has(curProblemId) && solvedCount !== cses.problems.length)
+            curProblemId = randomIndex();
+        setIndex(curProblemId);
+    };
+
+    const settingSolved = () => {
+        setSolved((solved) => solved.add(cses.problems[index].pid));
+        setSolvedCount((count) => count + 1);
+        settingIndex();
+    };
+
+    return (
+        <>
+            <h1>Data Structures and Algorithms</h1>
+            <h2>
+                <a
+                    href={csesWebAddress.concat(
+                        String(cses.problems[index].pid)
+                    )}>
+                    {cses.problems[index].problem}
+                </a>
+            </h2>
+            <div className="card">
+                <button onClick={settingIndex}>Too Hard!!</button>
+                <button className="success" onClick={settingSolved}>
+                    Accepted
+                </button>
+            </div>
+            <h1 className="count">{solvedCount}</h1>
+        </>
+    );
 }
 
-export default App
+export default App;
